1c76a9e0e459b6720108f9f0ead3867ecaccada0
Hasinur Rahman <hasinurrahman3@gmail.com>
2026-02-13 15:44:13 +0600

    update: Updated CI pipeline workflows

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,23 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        $this->forceSqliteTestingDatabase();
+
+        parent::setUp();
+    }
+
+    private function forceSqliteTestingDatabase(): void
+    {
+        foreach ([
+            'APP_ENV' => 'testing',
+            'DB_CONNECTION' => 'sqlite',
+            'DB_DATABASE' => ':memory:',
+        ] as $key => $value) {
+            putenv("{$key}={$value}");
+            $_ENV[$key] = $value;
+            $_SERVER[$key] = $value;
+        }
+    }
 }
